bb5064e085a5ee30f400eb42a8fc4a63e0403d89
Varadjanin <mbodrozic@gmail.com>
2021-04-03 09:43:54 +0200

    adding more details

--- functions3.js
+++ functions3.js
@@ -214,22 +214,20 @@ function mostFrequentItem(arr) {
     var mf = 1;
     var m = 0;
     var item;
-    for (var i=0; i<arr1.length; i++)
-    {
-            for (var j=i; j<arr1.length; j++)
-            {
-                    if (arr1[i] == arr1[j])
-                     m++;
-                    if (mf<m)
-                    {
-                      mf=m; 
-                      item = arr1[i];
-                    }
+    for (var i=0; i<arr.length; i++) {
+        for (var j=i; j<arr.length; j++) {
+            if (arr[i] == arr[j]) {
+                m++;
             }
-            m=0;
+            if (mf<m) {
+                mf=m; 
+                item = arr[i];
+            }
+        }
+        m=0;
     }
-    return item+" ( " +mf +" times ) ";
+    return item + " ( " + mf + " times ) ";
 }
 
-var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 2, 2, 2, 2, 2, 'a', 2, 4, 9, 3];
 console.log(mostFrequentItem(arr1));
