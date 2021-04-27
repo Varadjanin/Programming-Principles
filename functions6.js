edf7bd33cda89d151299abf1d3d1960215a70aa6
Varadjanin <mbodrozic@gmail.com>
2021-04-27 17:45:51 +0200

    small corrections

--- functions6.js
+++ functions6.js
@@ -342,4 +342,43 @@ function strInRectangularFrame(arr) {
 var proba = strInRectangularFrame(["Hello", "World", "in", "a", "frame","jos", "nesto", "najnovije"]);
 console.log(proba);
 
+//Dariovo resenje:
 
+function makeFrame(array) {
+	var maxWordLength = 0;
+	var topAndBottomString = '';
+	var topAndBottomlength = 0;
+	var middlePart = '';
+	var spaceLength = 0;
+	var spaceString = '';
+	var frame = '';
+	var word = '';
+	function makeStringFromChar(char, num) {
+		var resultString = '';
+		for (var i = 0; i < num; i++) {
+			resultString += char;
+		}
+		return resultString;
+	}
+	//find the longest word
+	for (var i = 0; i < array.length; i++) {
+		word = array[i];
+		if (word.length > maxWordLength) {
+			maxWordLength = word.length;
+		}
+	}
+	//make top and bottom strings
+	topAndBottomlength = maxWordLength + 4;
+	topAndBottomString = makeStringFromChar('*', topAndBottomlength);
+	//create middle part
+	for (var k = 0; k < array.length; k++) {
+		word = array[k];
+		spaceLength = maxWordLength - word.length;
+		spaceString = makeStringFromChar(' ', spaceLength);
+		middlePart += '* ' + word + spaceString + ' *\n';
+	}
+	//create frame
+	frame = topAndBottomString + '\n' + middlePart + topAndBottomString;
+	return frame;
+}
+console.log(makeFrame(['Hello', 'world', 'in', 'a', 'frame']))
